edd3dd818db09b5787ab1bf6ceb47bb9e9972395
asdiAdi <carltimothyadi@gmail.com>
2024-01-22 14:31:20 +0800

    fem first challenge init

---
+++ apps/huddle-landing-page-with-curved-sections/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="huddle-landing-page-with-curved-sections" />
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/limbo/README.md
@@ -0,0 +1,6 @@
+huddle-landing-page-with-curved-sections
+- vite
+- nextjs
+- scss
+- cypress
+- fem-1-junior
