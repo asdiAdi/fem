8d5f80b0373e6e9647575078737b44ffd430b3bc
asdiAdi <carltimothyadi@gmail.com>
2024-02-08 14:49:21 +0800

    huddle page init

--- apps/huddle-landing-page-with-curved-sections/src/app/app.tsx
+++ apps/huddle-landing-page-with-curved-sections/src/app/app.tsx
@@ -1,12 +1,30 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="huddle-landing-page-with-curved-sections" />
+    <div className={styles['test']}>
+      <nav>
+        <div>Huddle</div>
+        <button>Try It Free</button>
+      </nav>
+
+      <div>
+        <h1>Build The Community Your Fans Will Love</h1>
+        <p>
+          Huddle re-imagines the way we build communities. You have a voice, but
+          so does your audience. Create connections with your users as you
+          engage in genuine discussion.
+        </p>
+
+        <button>Get Started for Free</button>
+
+        {/* Image here */}
+
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 }
