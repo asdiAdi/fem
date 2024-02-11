4eb7aef6ef4bb597e069e501f6802c04fe276e59
asdiAdi <carltimothyadi@gmail.com>
2024-02-11 22:07:21 +0800

    added some styling

--- apps/huddle-landing-page-with-curved-sections/src/app/app.tsx
+++ apps/huddle-landing-page-with-curved-sections/src/app/app.tsx
@@ -1,22 +1,30 @@
 import styles from './app.module.scss';
+import logo from '../assets/logo.svg';
 
 export function App() {
   return (
     <div className={styles['test']}>
-      <nav>
-        <div>Huddle</div>
-        <button>Try It Free</button>
+      <nav className={styles['nav']}>
+        <img src={logo} className={styles['nav-logo']} alt="logo" />
+        {/*<div className={styles['nav-logo']}>*/}
+        {/*  <img src={iconMessages} alt="icon-messages" />*/}
+        {/*  <span>Huddle</span>*/}
+        {/*</div>*/}
+
+        <button className={styles['nav-button']}>Try It Free</button>
       </nav>
 
-      <div>
-        <h1>Build The Community Your Fans Will Love</h1>
-        <p>
+      <div className={styles['body']}>
+        <h1 className={styles['h1']}>
+          Build The Community Your Fans Will Love
+        </h1>
+        <p className={styles['body-description']}>
           Huddle re-imagines the way we build communities. You have a voice, but
           so does your audience. Create connections with your users as you
           engage in genuine discussion.
         </p>
 
-        <button>Get Started for Free</button>
+        <button className={styles['body-button']}>Get Started for Free</button>
 
         {/* Image here */}
 
