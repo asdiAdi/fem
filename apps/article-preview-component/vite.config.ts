6483ceebf3bef1e5365a52988e24a5413c60e843
asdiAdi <carltimothyadi@gmail.com>
2024-06-21 09:48:04 +0800

    article-preview-component init

---
+++ apps/article-preview-component/vite.config.ts
@@ -0,0 +1,43 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/article-preview-component',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  plugins: [react(), nxViteTsPaths()],
+  resolve: {
+    alias: {
+      src: '/src',
+      app: '/src/app',
+      assets: '/src/assets',
+      styles: '/src/styles',
+    },
+  },
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  build: {
+    outDir: '../../dist/apps/article-preview-component',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
